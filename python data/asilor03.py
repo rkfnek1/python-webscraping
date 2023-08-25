f147cb277d518df094e77c02c69cd6bbd1676fbf
rkfnek1 <80048872+rkfnek1@users.noreply.github.com>
2023-08-25 15:37:41 +0900

    Delete python data directory

--- python data/asilor03.py
+++
@@ -1,9 +0,0 @@
-import airtest
-from airtest.core.api import connect_device
-
-dev1 = connect_device("Android://127.0.0.1:5037/serialno1")
-dev2 = connect_device("Android://127.0.0.1:5037/serialno1")
-
-
-
-# current_dev = device()
